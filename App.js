6b94701565014275633d204d142691ed4633e72c
Pavan <kartik.varanasi@yahoo.com>
2024-02-20 17:51:26 +0530

    Initial SplashScreen/Font/Assets setup

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import IonIcons from "@expo/vector-icons/Ionicons";
+import { useAssets } from "expo-asset";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+   const [isAppReady, setIsAppReady] = useState(false);
+   const [assets] = useAssets([require("./test.jpg")]);
+   const [fontLoaded] = Font.useFonts(IonIcons.font);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   useEffect(() => {
+      const onInit = async () => {
+         try {
+            // Make API CALLS
+         } catch (error) {
+            // Catch API ERRORS
+         } finally {
+            setIsAppReady(true);
+         }
+      };
+      onInit();
+   }, []);
+
+   const onLayoutRootView = useCallback(async () => {
+      if (assets && fontLoaded && isAppReady) {
+         await SplashScreen.hideAsync();
+      }
+   }, [assets, fontLoaded, isAppReady]);
+
+   if (!fontLoaded || !assets) {
+      return null;
+   }
+
+   return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }} onLayout={onLayoutRootView}>
+         <StatusBar style="auto" />
+         <Text>SplashScreen Demo! 👋</Text>
+         <IonIcons name="logo-react" size={30} />
+      </View>
+   );
+}
