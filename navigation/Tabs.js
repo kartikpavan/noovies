06bb206d4fb832613331590bf307e2e6af913191
Pavan <kartik.varanasi@yahoo.com>
2024-02-20 19:15:27 +0530

    Colors

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -2,19 +2,28 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-import IonIcons from "@expo/vector-icons/Ionicons";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR, WHITE_COLOR, YELLOW_COLOR } from "../utils/colors";
 
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
+   const isDarkMode = useColorScheme() === "dark";
+
    return (
       <Tab.Navigator
          initialRouteName="Movies"
          screenOptions={{
-            tabBarActiveTintColor: "red",
-            tabBarInactiveTintColor: "purple",
+            tabBarActiveTintColor: isDarkMode ? YELLOW_COLOR : BLACK_COLOR,
+            tabBarInactiveTintColor: isDarkMode ? "#d2dae2" : "#808e9b",
+            tabBarStyle: { backgroundColor: isDarkMode ? BLACK_COLOR : WHITE_COLOR },
+            headerStyle: {
+               backgroundColor: isDarkMode ? BLACK_COLOR : WHITE_COLOR,
+            },
+            headerTitleStyle: {
+               color: isDarkMode ? "white" : BLACK_COLOR,
+            },
             headerTitleAlign: "center",
-            headerRight: () => <IonIcons name="search" size={24} color="green" />,
          }}>
          <Tab.Screen name="Movies" component={Movies} />
          <Tab.Screen name="TV" component={Tv} />
---
+++ utils/colors.js
@@ -0,0 +1,5 @@
+export const YELLOW_COLOR = "#ffa801";
+
+export const BLACK_COLOR = "#1e272e";
+
+export const WHITE_COLOR = "white";
