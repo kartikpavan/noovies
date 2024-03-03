02cf8dc1ea51c6a3ddf08a06235066fa71a2fe42
Pavan <kartik.varanasi@yahoo.com>
2024-03-03 23:19:51 +0530

    no Review error handling

--- screens/Reviews.tsx
+++ screens/Reviews.tsx
@@ -5,7 +5,7 @@ import { useReviews } from "../api/details";
 import { moviesURL, tvSeriesURL } from "../utils/constants";
 import IonIcons from "@expo/vector-icons/Ionicons";
 import { ActivityIndicator, FlatList } from "react-native";
-import { BLACK_COLOR, DARK_GREY, YELLOW_COLOR } from "../utils/colors";
+import { BLACK_COLOR, DARK_GREY, WHITE_COLOR, YELLOW_COLOR } from "../utils/colors";
 import Rating from "../components/Rating";
 import moment from "moment";
 
@@ -24,26 +24,34 @@ const Reviews: React.FC<NativeStackScreenProps<any, "Reviews">> = ({ route }) =>
    }
 
    return (
-      <FlatList
-         style={{ backgroundColor: BLACK_COLOR }}
-         data={reviews}
-         keyExtractor={(item, idx) => `${item.id}-${idx}`}
-         renderItem={({ item }) => (
-            <ReviewContainer>
-               <ReviewHeader>
-                  <IonIcons name="person" size={30} color="white" />
-                  <FlexColumn>
-                     <Author>{item.author}</Author>
-                     {item.author_details?.rating && (
-                        <Rating movieRating={item?.author_details?.rating} />
-                     )}
-                  </FlexColumn>
-               </ReviewHeader>
-               <Review>{item.content}</Review>
-               <Time>{moment(item.created_at).fromNow()}</Time>
-            </ReviewContainer>
+      <>
+         {reviews?.length === 0 ? (
+            <MainContainer>
+               <Text style={{ color: WHITE_COLOR, fontSize: 20 }}>No Reviews</Text>
+            </MainContainer>
+         ) : (
+            <FlatList
+               style={{ backgroundColor: BLACK_COLOR }}
+               data={reviews}
+               keyExtractor={(item, idx) => `${item.id}-${idx}`}
+               renderItem={({ item }) => (
+                  <ReviewContainer>
+                     <ReviewHeader>
+                        <IonIcons name="person" size={30} color="white" />
+                        <FlexColumn>
+                           <Author>{item.author}</Author>
+                           {item.author_details?.rating && (
+                              <Rating movieRating={item?.author_details?.rating} />
+                           )}
+                        </FlexColumn>
+                     </ReviewHeader>
+                     <Review>{item.content}</Review>
+                     <Time>{moment(item.created_at).fromNow()}</Time>
+                  </ReviewContainer>
+               )}
+            />
          )}
-      />
+      </>
    );
 };
 
@@ -52,6 +60,14 @@ export default Reviews;
 const LoaderContainer = styled.View`
    flex: 1;
    align-items: center;
+   justify-content: center;
+   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const MainContainer = styled.View`
+   flex: 1;
+   align-items: center;
+   padding: 0px 20px;
    justify-content: center;
    background-color: ${(props) => props.theme.mainBgColor};
 `;
@@ -65,6 +81,8 @@ const ReviewHeader = styled.View`
    gap: 20px;
    align-items: center;
 `;
+
+const Text = styled.Text``;
 
 const Author = styled.Text`
    font-weight: 600;
